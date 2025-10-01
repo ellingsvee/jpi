bb4a18d5f7f97a139953b96cd3ff46a850e73093
ellingsvee <elling.svee@gmail.com>
2025-10-01 22:57:52 +0200

    Update init

---
+++ src/jpi/interface/__init__.py
@@ -0,0 +1,5 @@
+from jpi.interface.allgather import allgather
+from jpi.interface.allreduce import allreduce
+from jpi.interface.bcast import bcast
+from jpi.interface.reduce import reduce
+from jpi.interface.scatter import scatter
